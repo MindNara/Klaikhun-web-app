88fa516a500cb8cd8357571095e1cf86cad43d92
TANADECH ZHENG <64070157@kmitl.ac.th>
2023-03-18 10:22:29 +0700

    update navbar

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="container flex flex-row items-center justify-between mx-auto pt-8 w-full" >
+            <div className="items-center justify-between space-x-20 w-full md:flex md:w-auto z-10">
+                <a href="#" className="flex items-center">                  
+                    <span className="self-center text-2xl font-bold whitespace-nowrap ml-2">KLAIKHUN</span>
+                </a>
+
+
+                <ul
+                    className="flex flex-col p-4 mt-4 md:flex-row md:space-x-14 md:mt-0 md:text-base md:font-medium md:border-0">
+                    <li>
+                        <a href="#" className="block py-2 pl-3 pr-4 rounded md:p-0" aria-current="page">Hotels</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            className="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">Flights</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            className="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">Deals</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="flex items-center md:order-2 z-10">
+                <span className="text-base font-medium mr-5">Hi, Member Name</span>
+                <button className="flex mr-3 text-sm md:mr-0">
+                    <div className="block bg-black rounded-lg h-10 w-10" ></div>
+                </button>
+            </div>
+    </nav>
+  )
+}
+
+export default Navbar
