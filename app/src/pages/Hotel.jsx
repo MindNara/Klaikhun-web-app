b529cd61a29f61049d5898a85487cc51642180cd
yyolradee <64070089@kmitl.ac.th>
2023-03-19 13:39:01 +0700

    hotfix: config color

--- app/src/pages/Hotel.jsx
+++ app/src/pages/Hotel.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { HotelSearch } from '../components/index'
+import React from "react";
+import { HotelSearch } from "../components/index";
 
 const Hotel = () => {
   return (
-    <div className='flex mt-[20em]'>
+    <>
+      <div className="flex">
         <HotelSearch />
-    </div>
-  )
-}
+      </div>
+      {/* filter */}
+      <div className="flex w-auto">
+        <div className="flex justify-between">
+          <h1>filter</h1>
+          <button className="bg-gray-3">Reset Filter</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Hotel
+export default Hotel;
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -6,12 +6,12 @@ module.exports = {
   theme: {
     colors: {
       'gray': {
-        '1': '626367',
-        '2': 'B2B2B2',
-        '3': 'F6F6F8',
-        '4': 'FEFEFE'
+        '1': '#626367',
+        '2': '#B2B2B2',
+        '3': '#F6F6F8',
+        '4': '#FEFEFE'
       },
-      'skyblue': 'DFF2FF'
+      'skyblue': '#DFF2FF'
     },
     extend: {},
   },
