163a7980d8138eab2c918ce011b0f0cecfc86a6c
yyolradee <64070089@kmitl.ac.th>
2023-03-20 17:40:37 +0700

    update: edit footer for some reason

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex justify-center text-center h-[100px] w-full bg-skyblue">
+        <footer className="flex justify-center items-center text-center h-[400px] w-full bg-skyblue">
             <h1 className="text-gray-1 mt-8">Database System Concept<br/>IT @KMITL</h1>
-        </div>
+        </footer>
     )
 }
 
