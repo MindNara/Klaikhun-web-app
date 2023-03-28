449b58e213ccd8fe0aa84f629d27738980a84940
yyolradee <64070089@kmitl.ac.th>
2023-03-28 16:15:16 +0700

    Update: prop in deal page

--- app/src/pages/Deal.jsx
+++ app/src/pages/Deal.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Navbar } from "../components";
 import { DealCard } from "../components/index";
+import promotion from "../constants/promotion.json";
 
 const Deal = () => {
+  // console.log(promotion)
   return (
     <>
       <div className="flex justify-center items-center">
         <Navbar />
       </div>
       <div className="container mx-auto my-24 mt-[180px] flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-medium mb-16">Today's Top Deals</h1>
-        <div className="flex flex-nowrap w-full gap-8">
-          <DealCard />
+        <h1 className="text-4xl font-medium mb-5">Today's Top Deals</h1>
+        <h1 className="text-xl mb-16 text-gray-1">use code in the black box for discount your booking</h1>
+        <div className="grid grid-cols-3 w-full gap-8">
+          {promotion.map((prop) => {
+            return <DealCard pro={prop} />;
+          })}
         </div>
       </div>
     </>
