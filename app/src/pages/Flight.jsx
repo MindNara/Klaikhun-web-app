9a2deef776a18151e98364c2b07dfb6b6747a822
yyolradee <64070089@kmitl.ac.th>
2023-03-28 18:59:24 +0700

    Update: move flight info to json file

--- app/src/pages/Flight.jsx
+++ app/src/pages/Flight.jsx
@@ -1,30 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import flights from "../constants/flights.json"
 import { FlightSearch, FlightCard } from "../components/index";
 
 const Flight = () => {
-  const flights = [
-    {
-      airline_name: "Air Asia",
-      airline_logo:
-        "https://logowik.com/content/uploads/images/674_airasia.jpg",
-      flight_beginning: "BKK",
-      flight_destination: "CNX",
-      time_departure: "11:30 PM",
-      time_arriving: "10:30 PM",
-      ticket_price: 1690,
-    },
-    {
-      airline_name: "Bangkok Airways",
-      airline_logo:
-        "https://logowik.com/content/uploads/images/bangkok-airways7551.jpg",
-      flight_beginning: "BKK",
-      flight_destination: "CNX",
-      time_departure: "11:30 PM",
-      time_arriving: "11:30 PM",
-      ticket_price: 2090,
-    },
-  ];
 
   return (
     <>
