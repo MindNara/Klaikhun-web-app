8833e6a008f9194744eb85fc6445536351b81d4f
MindNara <64070171@it.kmitl.ac.th>
2023-03-30 23:29:35 +0700

    update sign-in & sign-up

--- app/src/pages/Signin.jsx
+++ app/src/pages/Signin.jsx
@@ -4,21 +4,44 @@ const Signin = () => {
   return (
     <div>
       <div className=" w-full h-screen">
-        <div className="absolute bg-skyblue w-3/5 h-full"></div>
-        <div className="absolute bg-white w-2/5 h-full right-0 flex justify-center flex-col items-center p-20">
-            <h2 className="text-3xl font-bold text-center p-10">Sign in</h2>
-            <div className="w-full flex flex-col text-gray-2 py-2">
-              <label htmlFor="">User Name</label>
-              <input type="text" className="bg-white mt-2 p-2 border-b" />
+        <div className="absolute bg-skyblue w-1/2 h-full">
+          <div className="flex flex-col items-center justify-center h-full">
+            <a href="/" className="flex items-center mb-10">
+              <i class="fab fa-slack fa-2x" alt="Logo"></i>
+              <span className="self-center text-2xl font-bold whitespace-nowrap ml-2">
+                KLAIKHUN
+              </span>
+            </a>
+            <div className="flex flex-col items-center text-8xl max-2xl:text-7xl font-semibold mb-6">
+              <span>WELCOME BACK</span>
+              <span>TO KLAIKHUN</span>
             </div>
-            <div className="w-full flex flex-col text-gray-2 py-2">
-              <label htmlFor="">Password</label>
-              <input type="text" className="bg-white mt-2 p-2 border-balck border-b"/>
+            <p className="text-2xl max-2xl:text-xl font-light">Log in to continue to your account, Have fun.</p>
+
+            <div className="flex flex-col items-center mt-32">
+              <p className="text-xl font-medium mb-3">Don’t have an account?</p>
+              <a href="/signup" className="text-white bg-black px-8 py-2 text-xl rounded-3xl">SingUp</a>
             </div>
-              <button className="rounded-3xl w-full my-5 py-2 bg-black text-white">Submit</button>
+          </div>
+        </div>
+
+        <div className="absolute bg-white w-1/2 h-full right-0 flex justify-center flex-col items-center px-28">
+          <div className="flex flex-col items-center mb-20">
+            <h2 className="text-5xl font-bold text-center mb-3 uppercase">login Account</h2>
+            <span className="text-2xl font-light">Good to see you</span>
+          </div>
+          <div className="w-full flex flex-col text-gray-2 mb-8">
+            <label>User Name</label>
+            <input type="text" className="bg-white p-2 border-b" />
+          </div>
+          <div className="w-full flex flex-col text-gray-2">
+            <label>Password</label>
+            <input type="text" className="bg-white p-2 border-balck border-b" />
+          </div>
+          <button className="rounded-3xl text-lg w-full mt-20 py-3 bg-black text-white">Submit</button>
         </div>
       </div>
-    </div> 
+    </div>
   );
 };
 
--- app/src/pages/Signup.jsx
+++ app/src/pages/Signup.jsx
@@ -4,49 +4,69 @@ const Signup = () => {
   return (
     <div>
       <div className=" w-full h-screen">
-        <div className="absolute bg-white w-3/5 h-full flex justify-center flex-col items-center p-20">
-          <h2 className="text-3xl font-bold text-center p-10">Register</h2>
+        <div className="absolute bg-white w-1/2 h-full flex justify-center flex-col items-center p-20">
+          <h2 className="text-5xl font-bold text-center uppercase mb-16">Register</h2>
           <div className="w-full flex flex-row">
-            <div className="w-1/2 mr-1 text-gray-2 py-2 px-2">
-              <label htmlFor="">First Name</label>
+            <div className="w-1/2 mr-1 text-gray-2 py-2 px-2 mb-3">
+              <label>First Name</label>
               <input
                 type="text"
-                className="h-8 w-full  border-balck border-b text-sm p1-2 bg transparent pitline-blue-600 shadow-sm"
+                className="h-8 w-full border-balck border-b text-sm p-2 bg transparent pitline-blue-600 shadow-sm"
               />
             </div>
-
-            <div className="w-1/2 mr-1 text-gray-2 py-2 px-2">
-              <label htmlFor="">Last Name</label>
+            <div className="w-1/2 text-gray-2 py-2 px-2 mb-3">
+              <label>Last Name</label>
               <input
                 type="text"
-                className="h-8 w-full  border-balck border-b text-sm p1-2 bg transparent pitline-blue-600 shadow-sm"
+                className="h-8 w-full border-balck border-b text-sm p-2 bg transparent pitline-blue-600 shadow-sm"
               />
             </div>
           </div>
 
-          <div className="w-full flex flex-col text-gray-2 py-2 px-2">
-            <label htmlFor="">Username</label>
-            <input type="text" className="bg-white mt-2 p-2 border-b" />
+          <div className="w-full flex flex-col text-gray-2 py-2 px-2 mb-3">
+            <label>Username</label>
+            <input type="text" className="bg-white p-2 border-b" />
           </div>
-          <div className="w-full flex flex-col text-gray-2 py-2 px-2">
-            <label htmlFor="">Email</label>
+          <div className="w-full flex flex-col text-gray-2 py-2 px-2 mb-3">
+            <label>Email</label>
             <input
               type="text"
-              className="bg-white mt-2 p-2 border-balck border-b"
+              className="bg-white p-2 border-balck border-b"
             />
           </div>
-          <div className="w-full flex flex-col text-gray-2 py-2 px-2">
-            <label htmlFor="">Phone Number</label>
+          <div className="w-full flex flex-col text-gray-2 py-2 px-2 mb-3">
+            <label>Phone Number</label>
             <input
               type="text"
-              className="bg-white mt-2 p-2 border-balck border-b"
+              className="bg-white p-2 border-balck border-b"
             />
           </div>
-          <button className="rounded-3xl w-full my-5 py-2 px-2 bg-black text-white">
+
+          <button className="rounded-3xl text-lg w-full mt-10 py-3 bg-black text-white">
             Submit
           </button>
         </div>
-        <div className="absolute bg-skyblue w-2/5 h-full right-0"></div>
+
+        <div className="absolute bg-skyblue w-1/2 h-full right-0">
+          <div className="flex flex-col items-center justify-center h-full">
+            <a href="/" className="flex items-center mb-10">
+              <i class="fab fa-slack fa-2x" alt="Logo"></i>
+              <span className="self-center text-2xl font-bold whitespace-nowrap ml-2">
+                KLAIKHUN
+              </span>
+            </a>
+            <div className="flex flex-col items-center text-8xl max-2xl:text-7xl font-semibold mb-6">
+              <span>WELCOME</span>
+              <span>TO KLAIKHUN</span>
+            </div>
+            <p className="text-2xl max-2xl:text-xl font-light">Book with us! for a better experience.</p>
+
+            <div className="flex flex-col items-center mt-32">
+              <p className="text-xl font-medium mb-3">Already have an account?</p>
+              <a href="/signin" className="text-white bg-black px-8 py-2 text-xl rounded-3xl">SingIn</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
