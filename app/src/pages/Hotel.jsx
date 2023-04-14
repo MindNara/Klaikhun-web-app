d825925544cdd8a474ed2eabac1d2588971f3f51
yyolradee <64070089@kmitl.ac.th>
2023-04-14 16:44:16 +0700

    Update: filtered but dont use state

--- app/src/pages/Hotel.jsx
+++ app/src/pages/Hotel.jsx
@@ -45,6 +45,76 @@ const Hotel = () => {
     getHotels();
   }
 
+  const handleStar5Change = e => {
+    const isChecked = e.target.checked
+    params.five = isChecked
+    console.log(params.five)
+    getHotels();
+  }
+
+  const handleStar4Change = e => {
+    const isChecked = e.target.checked
+    params.four = isChecked
+    console.log(params.four)
+    getHotels();
+  }
+
+  const handleStar3Change = e => {
+    const isChecked = e.target.checked
+    params.three = isChecked
+    console.log(params.three)
+    getHotels();
+  }
+
+  const handleStar2Change = e => {
+    const isChecked = e.target.checked
+    params.two = isChecked
+    console.log(params.two)
+    getHotels();
+  }
+
+  const handleStar1Change = e => {
+    const isChecked = e.target.checked
+    params.one = isChecked
+    console.log(params.one)
+    getHotels();
+  }
+
+  const handleStarNoChange = e => {
+    const isChecked = e.target.checked
+    params.no = isChecked
+    console.log(params.no)
+    getHotels();
+  }
+
+  const handleCMChange = e => {
+    const isChecked = e.target.checked
+    params.cm = isChecked
+    console.log(params.cm)
+    getHotels();
+  }
+
+  const handleCBChange = e => {
+    const isChecked = e.target.checked
+    params.cb = isChecked
+    console.log(params.cb)
+    getHotels();
+  }
+
+  const handleKYChange = e => {
+    const isChecked = e.target.checked
+    params.ky = isChecked
+    console.log(params.ky)
+    getHotels();
+  }
+
+  const handlePBChange = e => {
+    const isChecked = e.target.checked
+    params.pb = isChecked
+    console.log(params.pb)
+    getHotels();
+  }
+
   useEffect(() => {
     getHotels();
   }, []);
@@ -99,7 +169,7 @@ const Hotel = () => {
             <h1 className="text-gray-1 text-xl">Star Rating</h1>
             <form className="flex flex-col gap-1 mt-2">
               <div>
-                <input type="checkbox" name="five" />
+                <input type="checkbox" name="five" onChange={handleStar5Change} />
                 <label className="ml-2" for="five">
                   <i className="fa-solid fa-star" />
                   <i className="fa-solid fa-star" />
@@ -109,7 +179,7 @@ const Hotel = () => {
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="four" />
+                <input type="checkbox" name="four" onChange={handleStar4Change} />
                 <label className="ml-2" for="four">
                   <i className="fa-solid fa-star" />
                   <i className="fa-solid fa-star" />
@@ -118,7 +188,7 @@ const Hotel = () => {
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="three" />
+                <input type="checkbox" name="three" onChange={handleStar3Change} />
                 <label className="ml-2" for="three">
                   <i className="fa-solid fa-star" />
                   <i className="fa-solid fa-star" />
@@ -126,20 +196,20 @@ const Hotel = () => {
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="two" />
+                <input type="checkbox" name="two" onChange={handleStar2Change} />
                 <label className="ml-2" for="two">
                   <i className="fa-solid fa-star" />
                   <i className="fa-solid fa-star" />
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="one" />
+                <input type="checkbox" name="one" onChange={handleStar1Change} />
                 <label className="ml-2" for="one">
                   <i className="fa-solid fa-star" />
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="no" />
+                <input type="checkbox" name="no" onChange={handleStarNoChange} />
                 <label className="text-gray-1 ml-2" for="no">
                   No Rating
                 </label>
@@ -185,25 +255,25 @@ const Hotel = () => {
             <h1 className="text-gray-1 text-xl">Location</h1>
             <form className="flex flex-col gap-1 mt-2">
               <div>
-                <input type="checkbox" name="location" value="CM" />
+                <input type="checkbox" name="location" value="CM" onChange={handleCMChange} />
                 <label className="text-gray-1 ml-2" for="CM">
                   Chiang Mai
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="location" value="CB" />
+                <input type="checkbox" name="location" value="CB" onChange={handleCBChange} />
                 <label className="text-gray-1 ml-2" for="CB">
                   Chon Buri
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="location" value="KY" />
+                <input type="checkbox" name="location" value="KY" onChange={handleKYChange} />
                 <label className="text-gray-1 ml-2" for="KY">
                   Khao Yai
                 </label>
               </div>
               <div>
-                <input type="checkbox" name="location" value="PB" />
+                <input type="checkbox" name="location" value="PB" onChange={handlePBChange} />
                 <label className="text-gray-1 ml-2" for="PB">
                   Prachin Buri
                 </label>
--- server/routes/hotel.js
+++ server/routes/hotel.js
@@ -7,19 +7,53 @@ router.get('/hotels', async function (req, res) {
     //filter from query string
     var min_price = req.query.minP || 0
     var max_price = req.query.maxP || 99999
-    var star5 = req.query.five ||  ''
-    var star4 = req.query.four || ''
-    var star3 = req.query.three || ''
-    var star2 = req.query.two || ''
-    var star1 = req.query.one || ''
-    var nostar = req.query.no || ''
+    var star5 = req.query.five === 'true' || false
+    var star4 = req.query.four  === 'true' || false
+    var star3 = req.query.three  === 'true' || false
+    var star2 = req.query.two  === 'true' || false
+    var star1 = req.query.one  === 'true' || false
+    var nostar = req.query.no  === 'true' || false
+    var haveStar = false
     var rating = req.query.rating || '0'
-    var location = req.query.location || ''
+    var cm = req.query.cm === 'true' || false
+    var cb = req.query.cb === 'true' || false
+    var ky = req.query.ky === 'true' || false
+    var pb = req.query.pb === 'true' || false
+    var class_filter = ''
+
+    if (star5 | star4 | star3 | star2 | star1 | nostar){
+        haveStar = true
+        if (star5){
+            class_filter += "5,"
+        }
+        if (star4){
+            class_filter += "4,"
+        }
+        if (star3) {
+            class_filter += "3,"
+        }
+        if (star2) {
+            class_filter += "2,"
+        }
+        if (star1) {
+            class_filter += "1,"
+        }
+        if (nostar) {
+            class_filter += "0,"
+        }
+        class_filter = class_filter.slice(0, -1)
+        console.log(class_filter)
+    }
 
     try {
 
         const [hotels, hotelsFields] = await pool.query(
-            "SELECT * FROM hotels WHERE review_score > ? AND room_price >= ? AND room_price <= ?", 
+            `SELECT * FROM hotels WHERE review_score > ? AND room_price >= ? AND room_price <= ?
+            ${haveStar ? `AND hotel_class IN (${class_filter})`: ""}
+            ${cm ? "AND hotel_location LIKE '%Chiang Mai%' OR hotel_location LIKE '%เชียงใหม่%'": ""}
+            ${cb ? "AND hotel_location LIKE '%Chon Buri%' OR hotel_location LIKE '%ชลบุรี%' OR hotel_location LIKE '%พัทยา%'": ""}
+            ${ky ? "AND hotel_location LIKE '%Khao Yai%' OR hotel_location LIKE '%เขาใหญ่%'": ""}
+            ${pb ? "AND hotel_location LIKE '%Prachin Buri%' OR hotel_location LIKE '%ปราจีนบุรี%'": ""}`, 
         [rating, min_price, max_price]);
 
         res.json({
